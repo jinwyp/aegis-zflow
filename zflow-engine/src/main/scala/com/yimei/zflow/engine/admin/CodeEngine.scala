4e79924e17c97a5a7df2d56f91483dc2f98cf853
hary <94093146@qq.com>
2017-01-12 14:47:05 +0800

    updated

--- zflow-engine/src/main/scala/com/yimei/zflow/engine/admin/CodeEngine.scala
+++ zflow-engine/src/main/scala/com/yimei/zflow/engine/admin/CodeEngine.scala
@@ -6,10 +6,12 @@ import java.nio.file.StandardOpenOption._
 import java.util.{HashMap => JMap}
 
 import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{FileIO, Keep, Source}
-import akka.stream.{ActorMaterializer, IOResult}
 import akka.util.ByteString
 import com.yimei.zflow.api.models.flow.{Edge, TaskInfo}
+import com.yimei.zflow.api.models.graph.{GraphConfig, GraphConfigProtocol}
 import freemarker.template.{Configuration, TemplateExceptionHandler}
 import org.apache.commons.io.FileUtils
 
@@ -18,7 +20,7 @@ import scala.concurrent.Future
 /**
   * Created by hary on 17/1/10.
   */
-object CodeEngine {
+object CodeEngine extends GraphConfigProtocol {
 
   import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -53,7 +55,7 @@ object CodeEngine {
     val meta = tdata.get("meta").asInstanceOf[CodeMeta]
     val entries = template.split("\\.").toList.init
 
-    val sets =  if (template.endsWith("Test.ftl")) "test" else "main"
+    val sets = if (template.endsWith("Test.ftl")) "test" else "main"
     val dirs: List[String] = root :: project :: "src" :: sets :: "scala" :: (
       meta.groupId.split("\\.").toList ++: (meta.artifact :: {
         if (entries.size == 1) Nil else entries.init
@@ -70,10 +72,84 @@ object CodeEngine {
     runnable.run().map(result => (tdata.get("file"), result))
   }
 
-  //  def genAll() = {
-  //    val models = List(
-  //      new JMap[String, AnyRef](){ put("meta", null); put("models", models)}
-  //    )
-  //    // Future.traverse()
-  //  }
+  def genAll(gc: GraphConfig)(implicit system: ActorSystem) = {
+    import java.util.{HashMap => JMap}
+
+    val httpExecutionContext = system.dispatcher
+    implicit val materializer = ActorMaterializer()
+
+    val meta = CodeMeta(gc.groupId, gc.artifact, gc.entry)
+    def m(tpl: String, file: String, code: AnyRef) = (tpl, new JMap[String, AnyRef] {
+      put("meta", meta)
+      put("file", file)
+      put("code", code)
+    })
+    def ms(tpl: String, filecodes: List[(String, AnyRef)]) =
+      filecodes.map(e => (tpl, new JMap[String, AnyRef] {
+        put("meta", meta)
+        put("file", e._1)
+        put("code", e._2)
+      }))
+
+    val all =
+      m("XFlowApp.ftl", s"${gc.entry}App.scala", getAppCode(gc)) ::
+        m("XFlowGraph.ftl", s"${gc.entry}Graph.scala", getGraphCode(gc)) ::
+        m("utask.XFlowModels.ftl", s"${gc.entry}Models.scala", getModelsCode(gc)) ::
+        m("XFlowGraph.ftl", s"${gc.entry}Config.scala", getConfigCode(gc)) ::
+        m("config.XFlowPoints.ftl", s"${gc.entry}Points.scala", getPointsCode(gc)) ::
+        m("utask.UTaskRoute.ftl", s"UTaskRoute.scala", getUTaskRouteCode(gc)) ::
+        (ms("utask.XFlowTask.ftl", getUTaskCode(gc)) ++
+          ms("utask.TaskTest.ftl", getUTaskTestCode(gc)))
+
+    Future.traverse(all) { entry =>
+      CodeEngine.genFile(entry._1, entry._2, "/tmp", s"aegis-zflow-${gc.artifact}")
+    }.foreach(_.foreach {
+      case (filename, result) =>
+        println(s"${filename} => ${result.status}, ${result.count} bytes read.")
+    })
+  }
+
+  def getAppCode(graphConfig: GraphConfig) = null
+
+  def getGraphCode(graphConfig: GraphConfig) =
+    CodeConfig(
+      new JMap[String, String] {
+        put("a", "a");
+        put("b", "b")
+      },
+      new JMap[String, String] {
+        put("V", "V");
+        put("W", "W")
+      },
+      new JMap[String, Edge] {
+        put("E1", Edge(name = "E1", end = "end"))
+      },
+      new JMap[String, TaskInfo] {
+        put("U1", TaskInfo("U1", List("a", "b", "c")))
+      },
+      new JMap[String, TaskInfo] {
+        put("A1", TaskInfo("A1", List("X", "Y", "Z")))
+      }
+    )
+
+  def getModelsCode(graphConfig: GraphConfig) = (null)
+
+  def getConfigCode(graphConfig: GraphConfig) = (getGraphCode(graphConfig))
+
+  def getPointsCode(graphConfig: GraphConfig) = (
+    CodePoints(new JMap[String, String] {
+      put("a", "a");
+      put("b", "b")
+    })
+    )
+
+  def getUTaskCode(graphConfig: GraphConfig) = {
+    List("TaskHello.scala" -> "Hello")
+  }
+
+  def getUTaskTestCode(graphConfig: GraphConfig) = {
+    List("TaskHelloTest.scala" -> "Hello")
+  }
+
+  def getUTaskRouteCode(graphConfig: GraphConfig) = (Array("T1", "T2"))
 }
--- zflow-single/src/main/scala/com/yimei/zflow/single/FlowApp.scala
+++ zflow-single/src/main/scala/com/yimei/zflow/single/FlowApp.scala
@@ -10,7 +10,7 @@ import akka.http.scaladsl.server.directives.LogEntry
 import akka.util.Timeout
 import com.yimei.zflow.api.GlobalConfig._
 import com.yimei.zflow.api.models.flow.{Edge, TaskInfo}
-import com.yimei.zflow.api.models.graph.GraphConfig
+import com.yimei.zflow.api.models.graph.{GraphConfig, GraphConfigProtocol}
 import com.yimei.zflow.engine.admin.CodeEngine.{CodeConfig, CodeMeta, CodePoints}
 import com.yimei.zflow.engine.admin.{AdminRoute, CodeEngine}
 import com.yimei.zflow.engine.graph.GraphLoader
@@ -21,212 +21,14 @@ import com.yimei.zflow.util.{FlowExceptionHandler, FlywayDB}
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
-
-class GenModule(system: ActorSystem) extends {
-  override implicit val coreSystem: ActorSystem = system
-} with Core {
-
-  implicit val httpExecutionContext = coreSystem.dispatcher
-
-  import java.util.{HashMap => JMap}
-
-  def genAll(gc: GraphConfig) = {
-    val meta = CodeMeta(gc.groupId, gc.artifact, gc.entry)
-    def m(tpl: String, file: String, code: AnyRef) = (tpl, new JMap[String, AnyRef] {
-      put("meta", meta)
-      put("file", file)
-      put("code", code)
-    })
-    def ms(tpl: String, filecodes: List[(String, AnyRef)]) =
-      filecodes.map(e => (tpl, new JMap[String, AnyRef] {
-        put("meta", meta)
-        put("file", e._1)
-        put("code", e._2)
-      }))
-
-    val all =
-      m("XFlowApp.ftl",           s"${gc.entry}App.scala",    getAppCode(gc))    ::
-      m("XFlowGraph.ftl",         s"${gc.entry}Graph.scala",  getGraphCode(gc))  ::
-      m("utask.XFlowModels.ftl",  s"${gc.entry}Models.scala", getModelsCode(gc)) ::
-      m("XFlowGraph.ftl",         s"${gc.entry}Config.scala", getConfigCode(gc)) ::
-      m("config.XFlowPoints.ftl", s"${gc.entry}Points.scala", getPointsCode(gc)) ::
-      m("utask.UTaskRoute.ftl",   s"UTaskRoute.scala",  getUTaskRouteCode(gc)) ::
-      ( ms("utask.XFlowTask.ftl", getUTaskCode(gc)) ++
-        ms("utask.TaskTest.ftl", getUTaskTestCode(gc)) )
-
-    Future.traverse(all) { entry =>
-      CodeEngine.genFile(entry._1, entry._2, "/tmp", s"aegis-zflow-${gc.artifact}" )
-    }.foreach(_.foreach{
-      case (filename, result) =>
-        println(s"${filename} => ${result.status}, ${result.count} bytes read.")
-    })
-  }
-
-  def getAppCode(graphConfig: GraphConfig) =  null
-
-  def getGraphCode(graphConfig: GraphConfig) =
-    CodeConfig(
-      new JMap[String, String] { put("a", "a"); put("b", "b") },
-      new JMap[String, String] { put("V", "V"); put("W", "W") },
-      new JMap[String, Edge] { put("E1", Edge(name = "E1", end = "end")) },
-      new JMap[String, TaskInfo] { put("U1", TaskInfo("U1", List("a", "b", "c"))) },
-      new JMap[String, TaskInfo] { put("A1", TaskInfo("A1", List("X", "Y", "Z"))) }
-    )
-
-  def getModelsCode(graphConfig: GraphConfig) = (null)
-  def getConfigCode(graphConfig: GraphConfig) = (getGraphCode(graphConfig))
-  def getPointsCode(graphConfig: GraphConfig) = (
-    CodePoints(new JMap[String, String] {
-      put("a", "a"); put("b", "b")
-    })
-    )
-
-  def getUTaskCode(graphConfig: GraphConfig) = {
-    List("TaskHello" -> "Hello")
-  }
-
-  def getUTaskTestCode(graphConfig: GraphConfig) = {
-    List("TaskHelloTest" -> "Hello")
-  }
-
-  def getUTaskRouteCode(graphConfig: GraphConfig) = ( Array("T1", "T2"))
-
-  def gen() = {
-    val meta = CodeMeta("com.yimei.zflow", "money", "Money")
-    val code = new JMap[String, AnyRef]() {
-      put("meta", meta)
-      put("file", "MoneyApp.scala")
-    }
-    CodeEngine.genFile("XFlowApp.ftl", code, "/tmp", "aegis-zflow-money")
-      .foreach { case (filename, result) =>
-        println(s"${filename} => ${result.status}, ${result.count} bytes read.")
-      }
-
-    val codeModel = new JMap[String, AnyRef] {
-      put("meta", meta)
-      put("file", "Models.scala")
-    }
-    CodeEngine.genFile("utask.XFlowModels.ftl", codeModel, "/tmp", "aegis-zflow-money")
-      .foreach { case (filename, result) =>
-        println(s"${filename} => ${result.status}, ${result.count} bytes read.")
-      }
-
-    val codeConfig = new JMap[String, AnyRef] {
-      put("meta", meta)
-      put("file", "MoneyConfig.scala")
-      put("code", CodeConfig(
-        new JMap[String, String] {
-          put("a", "a"); put("b", "b")
-        },
-        new JMap[String, String] {
-          put("V", "V"); put("W", "W")
-        },
-        new JMap[String, Edge] {
-          put("E1", Edge(name = "E1", end = "end"))
-        },
-        new JMap[String, TaskInfo] {
-          put("U1", TaskInfo("U1", List("a", "b", "c")))
-        },
-        new JMap[String, TaskInfo] {
-          put("A1", TaskInfo("A1", List("X", "Y", "Z")))
-        }
-      ))
-    }
-    CodeEngine.genFile("config.XFlowConfig.ftl", codeConfig, "/tmp", "aegis-zflow-money")
-      .foreach { case (filename, result) =>
-        println(s"${filename} => ${result.status}, ${result.count} bytes read.")
-      }
-
-    val codeGraph = new JMap[String, AnyRef] {
-      put("meta", meta)
-      put("file", "MoneyGraph.scala")
-      put("code", CodeConfig(
-        new JMap[String, String] {
-          put("a", "a"); put("b", "b")
-        },
-        new JMap[String, String] {
-          put("V", "V"); put("W", "W")
-        },
-        new JMap[String, Edge] {
-          put("E1", Edge(name = "E1", end = "end"))
-        },
-        new JMap[String, TaskInfo] {
-          put("U1", TaskInfo("U1", List("a", "b", "c")))
-        },
-        new JMap[String, TaskInfo] {
-          put("A1", TaskInfo("A1", List("X", "Y", "Z")))
-        }
-      ))
-    }
-    CodeEngine.genFile("XFlowGraph.ftl", codeGraph, "/tmp", "aegis-zflow-money")
-      .foreach { case (filename, result) =>
-        println(s"${filename} => ${result.status}, ${result.count} bytes read.")
-      }
-
-
-    // 生成 config/MoneyPoints.scala
-
-    val codePoints = new JMap[String, AnyRef] {
-      put("meta", meta)
-      put("code", CodePoints(new JMap[String, String] {
-        put("a", "a"); put("b", "b")
-      }))
-      put("file", "MoneyPoints.scala")
-    }
-    CodeEngine.genFile("config.XFlowPoints.ftl", codePoints, "/tmp", "aegis-zflow-money")
-      .foreach { case (filename, result) =>
-        println(s"${filename} => ${result.status}, ${result.count} bytes read.")
-      }
-
-    // 生成 UTaskRoute
-    val codeUTaskRoute = new JMap[String, AnyRef] {
-      put("meta", meta)
-      put("code", Array("T1", "T2"))
-      put("file", "UTaskRoute.scala")
-    }
-    CodeEngine.genFile("utask.UTaskRoute.ftl", codeUTaskRoute, "/tmp", "aegis-zflow-money")
-      .foreach { case (filename, result) =>
-        println(s"${filename} => ${result.status}, ${result.count} bytes read.")
-      }
-
-    def getTask(taskName: String) = {
-
-      val codeTask = new JMap[String, AnyRef] {
-        put("meta", meta)
-        put("code", taskName)
-        put("file", s"Task${taskName}.scala")
-      }
-      CodeEngine.genFile(s"utask.XFlowTask.ftl", codeTask, "/tmp", "aegis-zflow-money")
-        .foreach { case (filename, result) =>
-          println(s"${filename} => ${result.status}, ${result.count} bytes read.")
-        }
-
-    }
-    getTask("Hello")
-
-    def getTaskTest(taskName: String) = {
-      val codeTask = new JMap[String, AnyRef] {
-        put("meta", meta)
-        put("code", taskName)
-        put("file", s"Task${taskName}Test.scala")
-      }
-      CodeEngine.genFile(s"utask.TaskTest.ftl", codeTask, "/tmp", "aegis-zflow-money")
-        .foreach { case (filename, result) =>
-          println(s"${filename} => ${result.status}, ${result.count} bytes read.")
-        }
-    }
-    getTaskTest("Hello")
-  }
-
-
-}
+import scala.io.Source
 
 /**
   * Created by hary on 17/1/6.
   */
 object FlowApp extends {
   implicit val coreSystem = ActorSystem("FlowSystem")
-} with App with FlowExceptionHandler with EngineRoute with OrganRoute with AdminRoute {
+} with App with FlowExceptionHandler with EngineRoute with OrganRoute with AdminRoute with GraphConfigProtocol{
 
   override val utaskTimeout = Timeout(3.seconds)
   override val flowServiceTimeout = Timeout(3.seconds)
@@ -254,7 +56,13 @@ object FlowApp extends {
     case _ => None // no log entries for rejections
   }
 
-  new GenModule(coreSystem).gen()
+  import spray.json._
+  val graphConfig = Source.fromInputStream(this.getClass.getClassLoader.getResourceAsStream("money.json"))
+    .mkString
+    .parseJson
+    .convertTo[GraphConfig]
+
+  CodeEngine.genAll(graphConfig)
 
   // prepare routes
   val route: Route =
