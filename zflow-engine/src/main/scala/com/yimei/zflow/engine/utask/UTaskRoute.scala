e63a66c2cf8051f2e4dbce139c709fd038123858
qwang1990 <526186564@qq.com>
2017-01-24 20:59:52 +0800

    fix

--- zflow-engine/src/main/scala/com/yimei/zflow/engine/utask/UTaskRoute.scala
+++ zflow-engine/src/main/scala/com/yimei/zflow/engine/utask/UTaskRoute.scala
@@ -3,11 +3,13 @@ package com.yimei.zflow.engine.utask
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import com.yimei.zflow.api.models.utask.UTaskProtocol
+import com.yimei.zflow.engine.utask.Models.UserSubmitEntity
 
 /**
   * Created by hary on 17/1/7.
   */
-trait UTaskRoute extends SprayJsonSupport with UTaskService {
+trait UTaskRoute extends SprayJsonSupport with UTaskService with UTaskProtocol {
 
   /**
     * 1. 查询任务列表    guid为可选参数, 如果没有就是查询所有用户, flowType可选, 没有就是查询所有流程类型
@@ -23,11 +25,11 @@ trait UTaskRoute extends SprayJsonSupport with UTaskService {
 
   /**
     * 2. 查询指定任务
-    * GET /utask/:taskId
+    * GET /utask/:guid
     */
   def queryTask = get {
-    path("utask" / Segment) { taskId =>
-      complete(s"queryTask: taskId = $taskId")
+    path("utask" / Segment) { guid =>
+      complete(utaskQuery(guid))
     }
   }
 
@@ -36,8 +38,10 @@ trait UTaskRoute extends SprayJsonSupport with UTaskService {
     * POST /utask/:taskId
     */
   def submitTask = post {
-    path("utask" / Segment) {  taskId =>
-      complete(s"submitTask: taskId = $taskId")
+    (path("utask" / Segment / Segment / Segment) & entity(as[UserSubmitEntity])) {  (guid, taskId, flowType, entity) =>
+
+
+      complete(utaskSubmit(guid,taskId,flowType,entity))
     }
   }
 
--- zflow-engine/src/main/scala/com/yimei/zflow/engine/utask/UTaskService.scala
+++ zflow-engine/src/main/scala/com/yimei/zflow/engine/utask/UTaskService.scala
@@ -1,41 +1,45 @@
 package com.yimei.zflow.engine.utask
 
+import java.sql.SQLIntegrityConstraintViolationException
+
 import akka.pattern._
 import akka.util.Timeout
-import com.yimei.zflow.api.models.flow.DataPoint
-import com.yimei.zflow.api.models.utask.{CommandCreateUser, CommandQueryUser, CommandTaskSubmit, State => UserState}
+import com.yimei.zflow.api.models.utask.{CommandQueryUser, CommandTaskSubmit, State => UserState}
 import com.yimei.zflow.engine.db.Entities.FlowTaskEntity
 import com.yimei.zflow.engine.db.FlowTaskTable
 import com.yimei.zflow.engine.utask.Models._
 import com.yimei.zflow.util.config.Core
+import com.yimei.zflow.util.exception.DatabaseException
+import spray.json._
 
 import scala.concurrent.Future
 
 /**
   * Created by hary on 17/1/6.
   */
-trait UTaskService extends FlowTaskTable with Core{
+trait UTaskService extends FlowTaskTable with Core {
 
   val utaskTimeout: Timeout
-  import driver.api._
-  import coreSystem.dispatcher
 
   import com.yimei.zflow.engine.FlowRegistry.utask
+  import coreSystem.dispatcher
+  import driver.api._
 
   // 1> 创建用户
   // 2> 查询用户
   // 3> 用户提交任务
-//  def utaskCreate(guid: String): Future[UserState] =
-//    if (utask != null) {
-//      (utask ? CommandCreateUser(guid)) (utaskTimeout).mapTo[UserState]
-//    } else {
-//      Future.failed(new Exception("utask is not prepared"))
-//    }
+  //  def utaskCreate(guid: String): Future[UserState] =
+  //    if (utask != null) {
+  //      (utask ? CommandCreateUser(guid)) (utaskTimeout).mapTo[UserState]
+  //    } else {
+  //      Future.failed(new Exception("utask is not prepared"))
+  //    }
 
-  private def toFlowTaskEntry(f:FlowTaskEntity) = FlowTaskEntry(f.flow_id,f.flow_type,f.task_id,f.task_name,f.task_submit,f.guid,f.ts_c)
+  private def toFlowTaskEntry(f: FlowTaskEntity) = FlowTaskEntry(f.flow_id, f.flow_type, f.task_id, f.task_name, f.task_submit, f.guid, f.ts_c)
 
   /**
     * 查询task list
+    *
     * @param guid
     * @param flowType
     * @param flowId
@@ -43,9 +47,9 @@ trait UTaskService extends FlowTaskTable with Core{
     * @param pageSize
     * @return
     */
-  def taskList(guid:Option[String], flowType:Option[String], flowId:Option[String], page:Option[Int], pageSize:Option[Int]): Future[ListTaskResponse] = {
+  def taskList(guid: Option[String], flowType: Option[String], flowId: Option[String], page: Option[Int], pageSize: Option[Int]): Future[ListTaskResponse] = {
     val (pg, pz) = (page, pageSize) match {
-      case (Some(p), Some(s)) if p >=1 && s >= 1 => (p, s)
+      case (Some(p), Some(s)) if p >= 1 && s >= 1 => (p, s)
       case _ => (1, 10)
     }
 
@@ -59,18 +63,23 @@ trait UTaskService extends FlowTaskTable with Core{
         .getOrElse(true: Rep[Boolean]))
 
 
-    val tasks: Future[Seq[FlowTaskEntity]] = dbrun(query.drop((pg-1) * pz).take(pz).result)
+    val tasks: Future[Seq[FlowTaskEntity]] = dbrun(query.drop((pg - 1) * pz).take(pz).result)
     val total = dbrun(query.length.result)
 
     for {
       ts <- tasks
-      t  <- total
-    } yield ListTaskResponse(ts.map(toFlowTaskEntry(_)),t)
-
+      t <- total
+    } yield ListTaskResponse(ts.map(toFlowTaskEntry(_)), t)
 
 
   }
 
+  /**
+    * 查询指定用户任务
+    *
+    * @param guid
+    * @return
+    */
   def utaskQuery(guid: String) =
     if (utask != null) {
       (utask ? CommandQueryUser(guid)) (utaskTimeout).mapTo[UserState]
@@ -78,9 +87,29 @@ trait UTaskService extends FlowTaskTable with Core{
       Future.failed(new Exception("utask is not prepared"))
     }
 
-  def utaskSubmit(guid: String, taskId: String, points: Map[String, DataPoint]) =
+
+  /**
+    * 提交指定任务
+    * @param guid
+    * @param taskId
+    * @param flowType
+    * @param entity
+    * @return
+    */
+  def utaskSubmit(guid: String, taskId: String, flowType: String, entity: UserSubmitEntity): Future[UserState] =
     if (utask != null) {
-      (utask ? CommandTaskSubmit(guid, taskId, points)) (utaskTimeout).mapTo[UserState]
+      def insertTask(s: UserState): Future[FlowTaskEntity] = {
+        dbrun(flowTask returning flowTask.map(_.id) into ((fl, id) => fl.copy(id = id)) +=
+          FlowTaskEntity(None, entity.flowId, flowType, taskId, entity.taskName, entity.points.toJson.toString, guid, None)
+        ) recover {
+          case a: SQLIntegrityConstraintViolationException => throw DatabaseException("当前任务已被提交")
+        }
+      }
+      for {
+        s <- (utask ? CommandTaskSubmit(guid, taskId, entity.points)) (utaskTimeout).mapTo[UserState]
+        ft <- insertTask(s)
+      } yield s
+
     } else {
       Future.failed(new Exception("utask is not prepared"))
     }
