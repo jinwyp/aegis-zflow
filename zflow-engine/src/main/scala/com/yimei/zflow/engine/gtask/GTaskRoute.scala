5206d06d8c26d940a5a366fb9cf649663e37f926
qwang1990 <526186564@qq.com>
2017-02-03 10:48:10 +0800

    gtask

--- zflow-engine/src/main/scala/com/yimei/zflow/engine/gtask/GTaskRoute.scala
+++ zflow-engine/src/main/scala/com/yimei/zflow/engine/gtask/GTaskRoute.scala
@@ -2,11 +2,12 @@ package com.yimei.zflow.engine.gtask
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.server.Directives._
+import com.yimei.zflow.api.models.gtask.GTaskProtocol
 
 /**
   * Created by hary on 17/1/7.
   */
-trait GTaskRoute extends SprayJsonSupport with GTaskService {
+trait GTaskRoute extends SprayJsonSupport with GTaskService with GTaskProtocol {
 
 
   /**
@@ -14,21 +15,28 @@ trait GTaskRoute extends SprayJsonSupport with GTaskService {
     *
     * GET /gtask?ggid=:ggid& flowType=:flowType& page=:page& pageSize=:pageSize
     */
-  private def listTask = get {
-    path("gtask") {
-      parameters("ggid", "flowType", "page".as[Int], "pageSize".as[Int]) {(ggid, flowType, page, pageSize) =>
-        complete(s"ggid = $ggid, flowType = $flowType, page = $page, pageSize = $pageSize")
+//  private def listTask = get {
+//    path("gtask") {
+//      parameters("ggid", "flowType", "page".as[Int], "pageSize".as[Int]) {(ggid, flowType, page, pageSize) =>
+//        complete(s"ggid = $ggid, flowType = $flowType, page = $page, pageSize = $pageSize")
+//      }
+//    }
+//  }
+    private def listTask = get {
+      path("gtask") {
+        parameters("ggid", "flowType") {(ggid, flowType) =>
+          complete(gtaskQuery(ggid,flowType))
+        }
       }
     }
-  }
 
   /**
     * claim任务
     * GET /gtask/:taskId
     */
   private def claimTask = get {
-    path("gtask" / Segment) { taskId =>
-      complete(s"taskId = $taskId")
+    path("gtask" / Segment  / Segment / Segment ) { (guid,taskId,ggid) =>
+      complete(gtaskClaim(ggid,guid,taskId))
     }
   }
 
